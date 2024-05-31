b3e5f36522a5a5f0075c46e67d33eb27cd945c18
Viktoriya Lazareva <lazareva_vika2005@mail.ru>
2024-05-31 23:20:15 +0400

    fourth commit - changed fibonacci func from recursive to iterative and changed main again

--- mit_hometask.cpp
+++ mit_hometask.cpp
@@ -2,14 +2,21 @@
 
 using namespace std;
 
-int fib(int n){ //recursive way to create the Fibonacci sequence that starts from 1, 1,...
-    if (n == 0) return 0;
-    if (n == 1 || n == 2) return 1;
-    return fib(n-1) + fib(n-2);
+void fib(int n){ //iterative way to create the Fibonacci sequence that starts from 0, 1,...
+    int current_element = 1; int previous_element = 1;
+    if (n > 1) {
+        cout << "0"<<"\n"<< "1"<<"\n";
+        for (int counter = 2; counter < n; ++counter){
+            cout << current_element << "\n";
+            current_element +=previous_element;
+            previous_element = current_element - previous_element;
+        }
+    }
+    else cout<<"0";
 }
 
 int main(){
-    int n; cin >> n;
-    int answer = fib(n);
-    cout << answer;
+    int n; 
+    cout << "Enter n: "; cin >> n;
+    fib(n);
 }
