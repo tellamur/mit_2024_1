1a632474562e74cc79a6a4d2a1c3e67608c98d73
Viktoriya Lazareva <lazareva_vika2005@mail.ru>
2024-05-31 23:09:17 +0400

    second commit - added recursive fibonacci func and changed main

--- mit_hometask.cpp
+++ mit_hometask.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+int fib(int n){ //recursive way to create the Fibonacci sequence that starts from 0, 1,...
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
 int main(){
-    cout << "Hello, World!";
+    int n; cin >> n;
+    int cur_var = n - 1;
+    if (cur_var>=0){
+        int answer = fib(cur_var);
+        cout << answer;
+    }
+    else cout << "0";
 }
