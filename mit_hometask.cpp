09c9e48969a2f5ddffdc3cf506c30768c25f3a74
Viktoriya Lazareva <lazareva_vika2005@mail.ru>
2024-05-31 23:13:38 +0400

    third commit - changed sequence in fibonacci func and changed main

--- mit_hometask.cpp
+++ mit_hometask.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int fib(int n){ //recursive way to create the Fibonacci sequence that starts from 0, 1,...
+int fib(int n){ //recursive way to create the Fibonacci sequence that starts from 1, 1,...
     if (n == 0) return 0;
     if (n == 1 || n == 2) return 1;
     return fib(n-1) + fib(n-2);
@@ -10,10 +10,6 @@ int fib(int n){ //recursive way to create the Fibonacci sequence that starts fro
 
 int main(){
     int n; cin >> n;
-    int cur_var = n - 1;
-    if (cur_var>=0){
-        int answer = fib(cur_var);
-        cout << answer;
-    }
-    else cout << "0";
+    int answer = fib(n);
+    cout << answer;
 }
